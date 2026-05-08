f27b18bff0f49ad7ec4d1b7b3c034e402641f07d
gorm4063-cpu <gorm4063@gmail.com>
2026-05-08 13:59:07 +0400

    Refactor linked list implementation and merge function

--- 07-05-2026/linklists.cpp
+++ 07-05-2026/linklists.cpp
@@ -1,33 +1,129 @@
-
-#include <vector>
 #include <iostream>
 using namespace std;
 
-class node {
+class list
+{
 public:
-	int data;
-	node* next;
-	static node* addnode(int newdata , node* previusnode){
-        node* newnode = new node();
-        newnode->data = newdata ;
-        newnode->next = nullptr;
-        if(previusnode->next = nullptr){
-            previusnode->next = newnode;
+    list();
+    ~list();
+    int size = 0;
+    int getsize()
+    {
+        return size;
+    }
+    void push_back(int data);
+    int &operator[](const int index);
+
+private:
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data, Node *next = nullptr)
+        {
+            this->data = data;
+            this->next = next;
         }
-        return newnode ;
-	}
+    };
+    Node *head = nullptr;
 };
 
+list::list()
+{
+    size = 0;
+    head = nullptr;
+}
+list::~list()
+{
+    while (head != nullptr)
+    {
+        Node *current = head;
+        head = head->next;
+        delete current;
+    }
+}
+void list::push_back(int data)
+{
+    if (head == nullptr)
+    {
+        head = new Node(data);
+    }
+    else
+    {
+        Node *current = this->head;
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+        current->next = new Node(data);
+    }
+    size++;
+}
+int &list::operator[](const int index)
+{
+    int counter = 0;
+    Node *current = this->head;
+    while (current != nullptr)
+    {
+        if (counter == index)
+        {
+            return current->data;
+        }
+        else
+        {
+            current = current->next;
+            counter++;
+        }
+    }
+}
+void middleElement(list &linklist)
+{
+    int middle = linklist.getsize() / 2;
+    cout << "the middle elemnt of first linklist is- " << linklist[middle] << " which index is- " << middle << endl;
+}
+list merging(list &linklist, list &linklist2)
+{
+    list newlist;
+    for (int i = 0; i < linklist.getsize(); i++)
+    {
+        newlist.push_back(linklist[i]);
+    }
+    for (int i = 0; i < linklist2.getsize(); i++)
+    {
+        newlist.push_back(linklist2[i]);
+    }
+    return newlist;
+}
+void printlist(list &linklist)
+{
+    for (int i = 0; i < linklist.getsize(); i++)
+    {
+        cout << linklist[i] << endl;
+    }
+}
 
+int main()
+{
+    list linklist;
+    linklist.push_back(10);
+    linklist.push_back(12);
+    linklist.push_back(13);
+    linklist.push_back(14);
+    list linklist2;
+    linklist2.push_back(-4);
+    linklist2.push_back(-3);
+    cout << " first element is " << linklist[0] << endl;
+    cout << "first list" << endl;
+    printlist(linklist);
+    cout << "second list" << endl;
+    printlist(linklist2);
+    cout << "--------------------------------" << endl;
+    middleElement(linklist);
+    cout << "--------------------------------" << endl;
+    cout << "after merging two lists" << endl;
+    list mergelist = merging(linklist, linklist2);
+    printlist(mergelist);
 
-int main() {
-    node* ob1 = new node();
-    ob1->data = 1;
-    
-    node* ob2 = node::addnode(10,ob1);
-    node* ob3 = node::addnode(15,ob2);
-    
-    cout << ob2->data << "\t" << ob2->next;
-    
-	return 0;
+    return 0;
 }
